1df1025be2bbe6937367baa1a2cbd1db792a5002
NandhuSathish <writetonandhusathish@gmail.com>
2025-03-16 09:05:26 +0530

    express added in the project

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,15 @@
-console.log('ts server running');
-// const app = ()=>{
+import app from './app';
+import { Config } from './config';
 
-// }
+const startServer = () => {
+    const port = Config.PORT;
+    try {
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}`),
+        );
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+startServer();
